f330732204d622570fa9345688bfbd372277ab93
Xavier T <xavopeus@yahoo.fr>
2022-11-29 08:38:50 +0100

    Still not work

--- js/user_script.js
+++ js/user_script.js
@@ -75,10 +75,10 @@ function uploadAppIcon() {
               <div class='w-full flex flex-col space-y-5 pt-8'>
                 <div class='flex flex-col space-y-1 flex-1 relative pb-7'>
                   <label for='appicon' class='font-semibold'>Téléchargez votre icône</label>
-                  <input id='appicon' type='file' name='appicon' accept='.png, .jpg, .jpeg, .svg' class='dark:bg-zinc-700 dark:border-zinc-900 dark:text-white block p-4 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary focus:border-primary' placeholder='Upload icon' />
+                  <input id='appicon' type='file' name='appicon' accept='.png, .jpg, .jpeg, .gif, .svg' class='dark:bg-zinc-700 dark:border-zinc-900 dark:text-white block p-4 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary focus:border-primary' placeholder='Upload icon' />
                 </div>
                 <div class='flex justify-end pt-7'>
-                  <input type='submit' id='uploadappiconform' name='uploadappiconform' class='cursor-pointer px-4 py-2 rounded flex space-x-2 hover:bg-gradient-to-r hover:from-black/5 hover:to-black/10 dark:hover:from-white/5 dark:hover:to-white/10 items-center text-white bg-[#335bff]' value='Valider' />
+                  <input type='submit' name='submit' class='cursor-pointer px-4 py-2 rounded flex space-x-2 hover:bg-gradient-to-r hover:from-black/5 hover:to-black/10 dark:hover:from-white/5 dark:hover:to-white/10 items-center text-white bg-[#335bff]' value='Valider' />
                 </div>
                 <div id='uploadIconMsg'></div>
               </div>
@@ -90,7 +90,6 @@ function uploadAppIcon() {
     </div>
   </div>
   `;
-
 }
 
 function uploadAppIconResult(msg_insert) {
--- php/upload.app.icon.php
+++ php/upload.app.icon.php
@@ -1,57 +1,110 @@
 
 
 
-<?php 
-  if (isset($_POST['uploadappiconform'])) {
-    if (is_uploaded_file($_FILES['appicon']['tmp_name'])) {
-      // First, Validate the file name
-      if(empty($_FILES['appicon']['name'])) {
-        var_dump('1');
-        echo '<script type="text/javascript">
-                msg_insert = "File name is empty!";
-                window.location = "../index.htm"
-                uploadAppIconResult(msg_insert);
-              </script>';
-        exit;
+
+
+<?php
+
+  if (isset($_POST['submit'])) {
+    if (isset($_FILES['appicon'])) {
+      var_dump($_FILES);
+      $currentDirectory = getcwd();
+      $uploadDirectory = '/uploads/';
+    
+      $errors = []; // Store errors here
+    
+      $fileExtensionsAllowed = ['jpeg','jpg','png', 'gif', 'svg']; // These will be the only file extensions allowed 
+    
+      $separator = '.';
+      $fileName = $_FILES['appicon']['name'];
+      $fileSize = $_FILES['appicon']['size'];
+      var_dump($fileSize);
+      $fileTmpName  = $_FILES['appicon']['full_path'];
+      var_dump($fileTmpName);
+      $fileType = $_FILES['appicon']['type'];
+      var_dump($fileType);
+      $fileExplode = explode($separator, $fileName);
+      $fileExtension = end($fileExplode);
+      $fileExtToLower = strtolower($fileExtension);
+    
+      $uploadPath = $currentDirectory . $uploadDirectory . basename($fileName);
+
+      if (! in_array($fileExtToLower, $fileExtensionsAllowed)) {
+        $errors[] = "This file extension is not allowed. Please upload a JPEG, JPG, PNG, GIF or SVG file";
       }
-
-      $upload_file_name = $_FILES['appicon']['name'];
-      // Too long file name?
-      if(strlen ($upload_file_name) > 100) {
-        var_dump('2');
-        echo '<script type="text/javascript">
-                msg_insert = "Too long file name!";
-                window.location = "../index.htm"
-                uploadAppIconResult(msg_insert);
-              </script>';
-        exit;
+  
+      if ($fileSize > 4000000) {
+        $errors[] = "File exceeds maximum size (4MB)";
       }
-
-      // Replace any non-alpha-numeric cracters in th file name
-      $upload_file_name = preg_replace('/[^A-Za-z0-9 \.\-_]/', '', $upload_file_name);
-
-      // set a limit to the file upload size
-      if ($_FILES['appicon']['size'] > 1000000) {
-        var_dump('3');
-        echo '<script type="text/javascript">
-                msg_insert = "Too big file!";
-                window.location = "../index.htm"
-                uploadAppIconResult(msg_insert);
-              </script>';
-        exit;
-      }
-
-      // Save the file
-      $dest=__DIR__.'/uploads/'.$upload_file_name;
-      if (move_uploaded_file($_FILES['appicon']['tmp_name'], $dest)) {
-        var_dump('4');
-        echo '<script type="text/javascript">
-                msg_insert = "File Has Been Uploaded!";
-                window.location = "../index.htm"
-                uploadAppIconResult(msg_insert);
-              </script>';
+  
+      if (empty($errors)) {
+        $didUpload = move_uploaded_file($fileTmpName, $uploadPath);
+        var_dump($didUpload);
+  
+        if ($didUpload) {
+          echo "The file " . basename($fileName) . " has been uploaded";
+        } 
+        else {
+          echo "An error occurred. Please contact the administrator.";
+        }
+      } 
+      else {
+        foreach ($errors as $error) {
+          echo $error . "These are the errors" . "\n";
+        }
       }
     }
   }
 
+  // $target_dir = "uploads/";
+  // $target_file = $target_dir . basename($_FILES["appicon"]["name"]);
+  // $uploadOk = 1;
+  // $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+  // // Check if image file is a actual image or fake image
+  // if (isset($_POST["submit"])) {
+  //   $check = getimagesize($_FILES["appicon"]["tmp_name"]);
+  //   if($check !== false) {
+  //     echo "File is an image - " . $check["mime"] . ".";
+  //     $uploadOk = 1;
+  //   } 
+  //   else {
+  //     echo "File is not an image.";
+  //     $uploadOk = 0;
+  //   }
+  // }
+
+  // // Check if file already exists
+  // if (file_exists($target_file)) {
+  //   echo "Sorry, file already exists.";
+  //   $uploadOk = 0;
+  // }
+
+  // // Check file size
+  // if ($_FILES["appicon"]["size"] > 500000) {
+  //   echo "Sorry, your file is too large.";
+  //   $uploadOk = 0;
+  // }
+
+  // // Allow certain file formats
+  // if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+  // && $imageFileType != "gif" && $imageFileType != "svg") {
+  //   echo "Sorry, only JPG, JPEG, PNG, GIF & SVG files are allowed.";
+  //   $uploadOk = 0;
+  // }
+
+  // // Check if $uploadOk is set to 0 by an error
+  // if ($uploadOk == 0) {
+  //   echo "Sorry, your file was not uploaded.";
+  // // if everything is ok, try to upload file
+  // } 
+  // else {
+  //   if (move_uploaded_file($_FILES["appicon"]["tmp_name"], $target_file)) {
+  //     echo "The file ". htmlspecialchars( basename( $_FILES["appicon"]["name"])). " has been uploaded.";
+  //   } 
+  //   else {
+  //     echo "Sorry, there was an error uploading your file.";
+  //   }
+  // }
 ?>
+
