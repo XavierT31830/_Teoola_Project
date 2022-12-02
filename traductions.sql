6379301a9f2d3ba0ca334c6d6208f573de4cf20f
Xavier T <xavopeus@yahoo.fr>
2022-12-02 15:13:37 +0100

    SQL

--- traductions.sql
+++ traductions.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 22 nov. 2022 à 15:06
+-- Généré le : ven. 02 déc. 2022 à 14:13
 -- Version du serveur : 8.0.31
 -- Version de PHP : 8.1.12
 
@@ -35,8 +35,18 @@ CREATE TABLE IF NOT EXISTS `applications` (
   `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `user_id` int NOT NULL,
   PRIMARY KEY (`id_app`),
+  UNIQUE KEY `title` (`title`),
   KEY `user_app` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=125 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `applications`
+--
+
+INSERT INTO `applications` (`id_app`, `title`, `description`, `content`, `user_id`) VALUES
+(17, 'Test1', 'Description', '', 5),
+(123, 'Test2', 'On test', '', 5),
+(124, 'Test3', 'This is an App and i wanted to do a big decription here to test it, so go on !', '', 5);
 
 -- --------------------------------------------------------
 
@@ -90,10 +100,18 @@ CREATE TABLE IF NOT EXISTS `users` (
   `last_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `first_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `email` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `pwd` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `pwd` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id_user`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id_user`, `last_name`, `first_name`, `email`, `pwd`) VALUES
+(5, 'Torrente', 'Xavier', 'xavopeus@yahoo.fr', '3189086edcfdabc915934fb8a8cd0d310cadd7a33855b2fcbef12e7b5b104258'),
+(6, 'Gaillard', 'Remy', 'remy@gmail.com', 'd4ee672562b7f048e8a783efaccffefe6945b8d3dc1277e4ab6fde9140827843');
 
 --
 -- Contraintes pour les tables déchargées
